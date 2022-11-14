8f2550ffa4f27f15ac5078939acb0a0ef2ab5706
Gabriel Valin <gabrielvalincontato@gmail.com>
2022-11-14 09:34:05 -0300

    feat: add prisma and @prisma/client

---
+++ src/Infra/Utils/Prisma/migrations/20221114120644_start_migrations/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "nick" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
